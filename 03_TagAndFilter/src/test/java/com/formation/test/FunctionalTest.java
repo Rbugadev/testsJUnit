15bdb1c615eeaf4bf4c5872f8e42cca2b67707b4
Roque rbugadev@gmail.com <rbugadev@gmail.com>
2019-04-25 17:01:22 +0200

    Proyecto 3  - Tags Include, exclude

---
+++ 03_TagAndFilter/src/test/java/com/formation/test/FunctionalTest.java
@@ -0,0 +1,19 @@
+package com.formation.test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("functional")
+class FunctionalTest {
+
+    @Test
+    void testOne() {
+        System.out.println("Functional Test 1");
+    }
+
+    @Test
+    void testTwo() {
+        System.out.println("Functional Test 2");
+    }
+}
+
---
+++ 03_TagAndFilter/src/test/java/com/formation/test/NonFunctionalTest.java
@@ -0,0 +1,34 @@
+package com.formation.test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("non-functional")
+class NonFunctionalTest {
+
+    @Test
+    @Tag("performance")
+    @Tag("load")
+    void testOne() {
+        System.out.println("Non-Functional Test 1 (Performance/Load)");
+    }
+
+    @Test
+    @Tag("performance")
+    @Tag("stress")
+    void testTwo() {
+        System.out.println("Non-Functional Test 2 (Performance/Stress)");
+    }
+
+    @Test
+    @Tag("security")
+    void testThree() {
+        System.out.println("Non-Functional Test 3 (Security)");
+    }
+
+    @Test
+    @Tag("usability")
+    void testFour() {
+        System.out.println("Non-Functional Test 4 (Usability)");
+    }
+}
