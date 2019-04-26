6f045bea677bb8cde303f796285ca4a768229972
Roque rbugadev@gmail.com <rbugadev@gmail.com>
2019-04-26 10:16:56 +0200

    redireccion hacia atras

--- 01_GettingStarted/src/main/java/com/formation/AutomatiqueScript.java
+++ 01_GettingStarted/src/main/java/com/formation/AutomatiqueScript.java
@@ -19,17 +19,21 @@ public class AutomatiqueScript {
 		if (driver.getTitle().equalsIgnoreCase("Google")) {
 			System.out.println("Je suis bien dans Google page");
 		}
-		
 		Thread.sleep(2000);
-		driver.navigate().to("https://www.bing.com");
 		
+		driver.navigate().to("https://www.bing.com");
 		if (driver.getTitle().equalsIgnoreCase("Bing")) {
 			System.out.println("Je suis bien chez Bing");
 		}
+		Thread.sleep(3000);
 		
+		driver.navigate().back();
+		if (driver.getTitle().equalsIgnoreCase("Google")) {
+			System.out.println("De retour chez Google");
+		}
 		Thread.sleep(5000);
+		
 		driver.close();
-
 	}
 
 }
